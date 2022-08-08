870ef1faa201020f9f65f2c552c353affff31154
amp <amp@thinx.gr>
2022-08-08 16:51:01 +0300

    xamarin styles

--- Archiver/Archiver/View/AlbumDetails.xaml.cs
+++ Archiver/Archiver/View/AlbumDetails.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Archiver.Model;
 using Archiver.ViewModel;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,6 +13,34 @@ namespace Archiver.View
         {
             InitializeComponent();
             BindingContext = new AlbumDetailsViewModel(album);
+        }
+
+        private async void ItemClicked(object sender, ItemTappedEventArgs args)
+        {
+            if (args.Item == null)
+                return;
+
+            //await Navigation.PushAsync(new ItemDetails(args.Item as Item));
+        }
+
+        private async void EditItemClicked(object sender, EventArgs args)
+        {
+            MenuItem mi = (MenuItem)sender;
+            Item item = mi.CommandParameter as Item;
+
+            //await Navigation.PushAsync(new EditItemPage(item));
+        }
+
+        private async void DeleteItemClicked(object sender, EventArgs args)
+        {
+            MenuItem mi = (MenuItem)sender;
+            Item item = mi.CommandParameter as Item;
+
+            DeleteAlbumViewModel dVm = new DeleteAlbumViewModel(item.Id);
+
+            int d = await dVm.DeleteAlbum();
+            //if (d > 0)
+              //  vmAlbum.Albums.Remove(Item);
         }
     }
 }
--- Archiver/Archiver/ViewModel/AlbumDetailsViewModel.cs
+++ Archiver/Archiver/ViewModel/AlbumDetailsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Archiver.Model;
 using System;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
 using Xamarin.Forms;
 
@@ -8,6 +9,7 @@ namespace Archiver.ViewModel
     public class AlbumDetailsViewModel
     {
         public Album Album { get; set; }
+        public ObservableCollection<Item> Items { get; set; }
         public int ItemQty { get; set; }
         public ICommand GetItemQtyCmd => new Command(GetItemQty);
 
@@ -16,6 +18,10 @@ namespace Archiver.ViewModel
         public AlbumDetailsViewModel(Album album)
         {
             Album = album.Clone();
+            Items = new ObservableCollection<Item>();
+            Items.Add(new Item { Name = "Item1" });
+            Items.Add(new Item { Name = "Item2" });
+            Items.Add(new Item { Name = "Item3" });
         }   
 
         private void GetItemQty()
