c102207fd65b02e43513ebe3cc7b4dae6d4796bf
Bekas Athanasios <bekasath@gmail.com>
2022-09-07 21:38:36 +0300

    7/9/22

--- Archiver/Archiver/View/HelpPage.xaml.cs
+++ Archiver/Archiver/View/HelpPage.xaml.cs
@@ -14,6 +14,7 @@ namespace Archiver.View
         public HelpPage()
         {
             InitializeComponent();
+            web.Source = "file:///android_asset/HtmlDoc.html";
         }
     }
 }
