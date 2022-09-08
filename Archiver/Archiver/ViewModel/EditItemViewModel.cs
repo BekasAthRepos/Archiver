3ab24b6ec1961f6021b4e4e7c5507b09f4a72451
amp <amp@thinx.gr>
2022-09-08 16:41:24 +0300

    final apk

--- Archiver/Archiver/ViewModel/EditItemViewModel.cs
+++ Archiver/Archiver/ViewModel/EditItemViewModel.cs
@@ -96,7 +96,7 @@ namespace Archiver.ViewModel
             {                
                 _imgSrc = ImageSource.FromStream(() => result.GetStream());
                 Item.ImgPath = result.Path;
-                OnPropertyChanged(nameof(_imgSrc));
+                OnPropertyChanged(nameof(Item));
 
             }
         }
@@ -143,7 +143,7 @@ namespace Archiver.ViewModel
 
             var newImage = Path.Combine(FileSystem.AppDataDirectory, result.Path);
             Item.ImgPath = Path.GetFullPath(newImage);
-            OnPropertyChanged(nameof(_imgSrc));
+            OnPropertyChanged(nameof(Item));
         }
     }
 }
